e7b4f8718ed1d5e173ce260a882f9cbeb8d71b22
shaunniel02 <shaunniel02@gmail.com>
2024-03-18 10:59:43 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ID QR Scanner',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late QRViewController _controller;
+  final GlobalKey _qrKey = GlobalKey(debugLabel: 'QR');
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('QR Code Scanner'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: QRView(
+                key: _qrKey,
+                onQRViewCreated: _onQRViewCreated,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      _controller = controller;
+      _controller.scannedDataStream.listen((scanData) {
+        // print('Scanned data: ${scanData.code}');
+      });
+    });
+  }
+}
