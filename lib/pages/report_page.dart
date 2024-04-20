3469aa0f596d6ebd5b4c5c16cad2b55c4469b6cf
Shaun Niel Ochavo <shaunniel02@gmail.com>
2024-04-20 20:39:41 +0800

    Add focus node and clear text function

--- lib/pages/report_page.dart
+++ lib/pages/report_page.dart
@@ -11,6 +11,8 @@ class _ReportPageState extends State<ReportPage> {
   List<List<String>> scans = [];
   List<List<String>> filteredScans = [];
   String searchQuery = '';
+  FocusNode searchFocusNode = FocusNode();
+  TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
@@ -48,130 +50,152 @@ class _ReportPageState extends State<ReportPage> {
         .where((scan) => scan.any((item) => item.contains(searchQuery)))
         .toList();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Reports'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            onPressed: _loadData,
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-        child: Column(
-          children: [
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  searchQuery = value;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: "Search",
-                hintText: "Search",
-                hintStyle: TextStyle(fontSize: 14.0),
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                ),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            Expanded(
-              child: CustomScrollView(
-                slivers: [
-                  SliverStickyHeader(
-                    header: ClipRRect(
-                      borderRadius: const BorderRadius.vertical(
-                          top: Radius.circular(16.0)),
-                      child: Container(
-                        height: 60.0,
-                        color: Colors.purple[100],
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        alignment: Alignment.centerLeft,
-                        child: const ListTile(
-                          title: Text(
-                            'QR Code',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          leading: Text('Date    ',
-                              style: TextStyle(color: Colors.black)),
-                          trailing: Text('Time',
-                              style: TextStyle(color: Colors.black)),
-                        ),
-                      ),
-                    ),
-                    sliver: SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                        (context, i) => Dismissible(
-                          key: UniqueKey(),
-                          onDismissed: (direction) async {
-                            // Store the item and its index before removing it.
-                            List<String> deletedScan = scans[i];
-                            int deletedIndex = i;
-
-                            // Remove the item from the list.
-                            scans.removeAt(i);
-
-                            // Convert the updated list back to a format that can be stored in SharedPreferences.
-                            List<String> updatedScanStrings =
-                                scans.map((scan) => scan.join(',')).toList();
-
-                            // Save the updated list to SharedPreferences.
-                            final prefs = await SharedPreferences.getInstance();
-                            prefs.setStringList('scans', updatedScanStrings);
-
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: const Text("Item dismissed"),
-                                action: SnackBarAction(
-                                  label: 'CANCEL',
-                                  onPressed: () async {
-                                    // Restore the deleted item.
-                                    scans.insert(deletedIndex, deletedScan);
-                                    List<String> restoredScanStrings = scans
-                                        .map((scan) => scan.join(','))
-                                        .toList();
-                                    await prefs.setStringList(
-                                        'scans', restoredScanStrings);
-                                    setState(() {});
-                                  },
-                                ),
-                              ),
-                            );
-
-                            setState(() {});
-                          },
-                          child: ListTile(
-                            title: Text(
-                              filteredScans[i][2],
-                              style: const TextStyle(
-                                color: Colors.purple,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            leading: Text(
-                              filteredScans[i][0],
-                            ),
-                            trailing: Text(filteredScans[i][1]),
-                          ),
-                        ),
-                        childCount: filteredScans.length,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Reports'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: _loadData,
             ),
           ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+          child: Column(
+            children: [
+              TextField(
+                controller: searchController,
+                focusNode: searchFocusNode,
+                onChanged: (value) {
+                  setState(() {
+                    searchQuery = value;
+                  });
+                },
+                onSubmitted: (value) {
+                  searchFocusNode.unfocus();
+                },
+                decoration: InputDecoration(
+                  labelText: "Search",
+                  hintText: "Search",
+                  hintStyle: const TextStyle(fontSize: 14.0),
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: searchController.text.isNotEmpty
+                      ? IconButton(
+                          icon: const Icon(Icons.close),
+                          onPressed: () {
+                            setState(() {
+                              searchController.clear();
+                              searchQuery = '';
+                            });
+                          },
+                        )
+                      : null,
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 16.0),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Expanded(
+                child: CustomScrollView(
+                  slivers: [
+                    SliverStickyHeader(
+                      header: ClipRRect(
+                        borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(16.0)),
+                        child: Container(
+                          height: 60.0,
+                          color: Colors.purple[100],
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          alignment: Alignment.centerLeft,
+                          child: const ListTile(
+                            title: Text(
+                              'QR Code',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            leading: Text('Date    ',
+                                style: TextStyle(color: Colors.black)),
+                            trailing: Text('Time',
+                                style: TextStyle(color: Colors.black)),
+                          ),
+                        ),
+                      ),
+                      sliver: SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                          (context, i) => Dismissible(
+                            key: UniqueKey(),
+                            onDismissed: (direction) async {
+                              // Store the item and its index before removing it.
+                              List<String> deletedScan = scans[i];
+                              int deletedIndex = i;
+
+                              // Remove the item from the list.
+                              scans.removeAt(i);
+
+                              // Convert the updated list back to a format that can be stored in SharedPreferences.
+                              List<String> updatedScanStrings =
+                                  scans.map((scan) => scan.join(',')).toList();
+
+                              // Save the updated list to SharedPreferences.
+                              final prefs =
+                                  await SharedPreferences.getInstance();
+                              prefs.setStringList('scans', updatedScanStrings);
+
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: const Text("Item dismissed"),
+                                  action: SnackBarAction(
+                                    label: 'CANCEL',
+                                    onPressed: () async {
+                                      // Restore the deleted item.
+                                      scans.insert(deletedIndex, deletedScan);
+                                      List<String> restoredScanStrings = scans
+                                          .map((scan) => scan.join(','))
+                                          .toList();
+                                      await prefs.setStringList(
+                                          'scans', restoredScanStrings);
+                                      setState(() {});
+                                    },
+                                  ),
+                                ),
+                              );
+
+                              setState(() {});
+                            },
+                            child: ListTile(
+                              title: Text(
+                                filteredScans[i][2],
+                                style: const TextStyle(
+                                  color: Colors.purple,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              leading: Text(
+                                filteredScans[i][0],
+                              ),
+                              trailing: Text(filteredScans[i][1]),
+                            ),
+                          ),
+                          childCount: filteredScans.length,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
